e71f489b0db519a8be99b6105c03170ce992cc00
Krunal Kaneria <prokillerkk@outlook.com>
2021-07-29 22:58:28 +0530

    Updated Thread Project And Added Thread Join and Thread sleep method

--- Concurrency/Threads/src/AnotherThread.java
+++ Concurrency/Threads/src/AnotherThread.java
@@ -4,9 +4,10 @@ public class AnotherThread extends Thread{
         System.out.println("Hello from " + currentThread().getName());
 
         try {
-            Thread.sleep(3000);;
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             System.out.println("Another thread woke me up");
+            return;
         }
 
         System.out.println("Three Seconds Are Passed and Now I'm Alive");
--- Concurrency/Threads/src/Main.java
+++ Concurrency/Threads/src/Main.java
@@ -1,31 +1,45 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from the main thread");
-
         Thread anotherThread = new AnotherThread();
-        anotherThread.setName("==Another Thread==");
+        anotherThread.setName("Another Thread");
         anotherThread.start();
 
-        new Thread() {
-            public void run() {
-                System.out.println("Hello from anonymous class thread..");
-            }
-        }.start();
-
-
+//        runnableThread.start();
+//        anotherThread.interrupt();
+//        System.out.println("Hello from the main thread");
+//
+//        Thread anotherThread = new AnotherThread();
+//        anotherThread.setName("==Another Thread==");
+//        anotherThread.start();
+//
+//        new Thread() {
+//            public void run() {
+//                System.out.println("Hello from anonymous class thread..");
+//            }
+//        }.start();
+//
+//
         Thread myAnonyMousRunnable = new Thread(new MyRunnable() {
             @Override
             public void run() {
                 System.out.println("Hello from anonymous class of runnable interface");
+                try{
+                    anotherThread.join(500);
+                    System.out.println("Another thread work Completed. I'm running again..");
+                } catch(InterruptedException e) {
+                    System.out.println("I couldn't wait after all. I was interrupted");
+                }
             }
+
         });
         myAnonyMousRunnable.start();
-
-
-        Thread myRunnable = new Thread(new MyRunnable());
-        myRunnable.start();
-
-        System.out.println("Hello again from main thread");
+//
+//
+//        Thread myRunnable = new Thread(new MyRunnable());
+//        myRunnable.start();
+////        anotherThread.interrupt();
+//
+//        System.out.println("Hello again from main thread");
     }
 }
